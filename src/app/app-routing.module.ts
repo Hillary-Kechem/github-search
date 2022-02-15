35114a8fd57b87401bf3ba4de9551ccce7f5a73a
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-15 17:22:37 +0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,3 +8,28 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import{HomeComponent} from '././home/home.component';
+import{UserComponent} from '././user/user.component';
+import{RepositoryComponent} from '././repository/repository.component';
+
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'repository', component: RepositoryComponent },
+
+
+  { path: '', redirectTo:'/home', pathMatch:'full' },
+  // { path: '', redirectTo:'/home', pathMatch:'full' },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
