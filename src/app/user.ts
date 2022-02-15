b00be7baa4d3e965bca1cf8ae844910000538d98
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-15 16:51:08 +0300

    added user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,8 @@
 export class User {
-    constructor( public name:string, public login:string, public avatar_url:string, public public_repos:number, public html_url:string, public created_at:Date, public followers:number,public following:number){
-        
+    //    public name: string;
+    //    public bio: string;
+    //    public login: string;
+    //    public followers:number;
+    //    public following:number;
+    
     }
-}
