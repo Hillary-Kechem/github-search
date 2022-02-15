07092b7042147fbbf3ae5fa0829023a232b913b4
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-15 16:59:32 +0300

    personal profile

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  personal: any;
+  repos: any;
+  user!: string;
+  constructor(private githubservice: GithubService) {   
+  }
+
+  ngOnInit(): void {
+    this.user="Hillary-Kechem"
+    
+    this.githubservice.findUserName(this.user)
+    this.githubservice.getPersonalInfo().subscribe(personal => {
+      console.log(personal);
+      this.personal = personal;
+    });
+
+
+    this.githubservice.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+
+
+  }
+
+}
